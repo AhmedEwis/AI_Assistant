6ce8420bdf9c856971b114290935d52f936e5b99
Ahmed Ewis <65680238+AhmedEwis@users.noreply.github.com>
2023-06-13 10:47:36 +0300

    Update Home.py

--- src/Home.py
+++ src/Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 #Config
-st.set_page_config(layout="wide", page_icon="💬", page_title="Robby | Chat-Bot 🤖")
+st.set_page_config(layout="wide", page_icon="💬", page_title="AI_Assistant | Chat-Bot 🤖")
 
 
 #Contact
